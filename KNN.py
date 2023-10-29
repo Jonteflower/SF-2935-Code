8c3dbdb575951147a0b9b75e8ca6bd0685bf9cb4
Jonathan Nairn Tallen <jonathannairn@hotmail.com>
2023-10-29 15:43:32 +0100

    changes

--- KNN.py
+++ KNN.py
@@ -5,12 +5,22 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from data_reader import read_training_data
 import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score, GridSearchCV  # <-- Import GridSearchCV
 
 # Preprocess the data
 def preprocess_data(df):
     scaler = MinMaxScaler()
     df_scaled = scaler.fit_transform(df)
     return pd.DataFrame(df_scaled, columns=df.columns)
+
+def optimal_k_using_gridsearch(x, y):
+    knn = KNeighborsClassifier()
+    param_grid = {'n_neighbors': np.arange(1, 51)}
+    knn_gscv = GridSearchCV(knn, param_grid, cv=5)
+    knn_gscv.fit(x, y)
+    
+    return knn_gscv.best_params_['n_neighbors']
+
 
 # Plot KNN performance
 def plot_knn_performance(x, y):
@@ -50,6 +60,9 @@ y_train = df_train['Label']
 
 # Plot performance
 #plot_knn_performance(x_train, y_train)
+
+# Grid search
+#optimal_k = optimal_k_using_gridsearch(x_train, y_train)
 
 # Train and evaluate model
 knn = KNeighborsClassifier(n_neighbors=8)
